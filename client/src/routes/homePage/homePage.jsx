ee8e7c6f7837ddd479b97df01d55ace13fb6e8ee
Akhil Jose <akhilmj369@gmail.com>
2024-06-29 00:05:56 +0530

    Update homePage.jsx

--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -12,12 +12,6 @@ function HomePage() {
       <div className="textContainer">
         <div className="wrapper">
           <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
-            explicabo suscipit cum eius, iure est nulla animi consequatur
-            facilis id pariatur fugit quos laudantium temporibus dolor ea
-            repellat provident impedit!
-          </p>
           <SearchBar />
           <div className="boxes">
             <div className="box">
